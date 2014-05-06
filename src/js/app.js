4f42cda977f8b5906f5678d783ebde366c50ba57
Rick Mohr <rmohr@azavea.com>
2014-05-06 17:31:02 -0400

    Demonstrate geolocation

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,25 @@ function initialize() {
 
 function start(e) {
     document.getElementById('helloButton').addEventListener('click', toggleHello);
+    document.getElementById('geolocateButton').addEventListener('click', geolocate);
 }
 
 function toggleHello (e) {
     var button = e.currentTarget;
     button.textContent = (button.textContent === 'Hello' ? 'Goodbye' : 'Hello');
 }
+
+function geolocate() {
+    navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
+}
+
+function onGeolocateSuccess(position) {
+    alert('Latitude:  ' + position.coords.latitude + '\n' +
+          'Longitude: ' + position.coords.longitude);
+};
+
+function onGeolocateError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
