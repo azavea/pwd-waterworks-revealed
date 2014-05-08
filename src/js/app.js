05a1c35397edb2c40fa3edfea023322890137049
Michael Tedeschi <mike.tedeschi@gmail.com>
2014-05-08 17:04:05 -0400

    Commit all prototype static files, sass project, and basic JS stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,105 @@
+var WR = WR || {};
 
-module.exports = {
-    initialize: function () {
-        var event = (window.device ? 'deviceready' : 'DOMContentLoaded'); // Allow launching in desktop browser
-        document.addEventListener(event, start, false);
-    }
-};
+WR.App = (function() {
+    'use strict';
 
-function start(e) {
-    document.getElementById('helloButton').addEventListener('click', toggleHello);
-    document.getElementById('geolocateButton').addEventListener('click', geolocate);
-}
+    // General UI stuff, including panel and pages
+    var UI = (function() {
+        var snap;
 
-function toggleHello (e) {
-    var button = e.currentTarget;
-    button.textContent = (button.textContent === 'Hello' ? 'Goodbye' : 'Hello');
-}
+        var initSnap = function() {
+            snap = new Snap({
+                element: document.getElementById('content'),
+                dragger: null,
+                disable: 'right',
+                addBodyClasses: true,
+                hyperextensible: false,
+                resistance: 0.5,
+                flickThreshold: 50,
+                transitionSpeed: 0.3,
+                easing: 'ease',
+                maxPosition: 266,
+                minPosition: -266,
+                tapToClose: true,
+                touchToDrag: false,
+                slideIntent: 40,
+                minDragDistance: 5
+            });
+        };
+        var toggleMenu = function() {
+            if( snap.state().state == "left" ){ 
+                snap.close();
+            } else {
+                snap.open('left');
+            }
+        };
+        var togglePages = function(e) {
+            e.preventDefault();
+            var target = $(this).attr('data-page');
+            var activePage = $('#content').find('.page.active').attr('id');
 
-function geolocate() {
-    navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
-}
+            if ( activePage === target ){}
+            else {
+                $('#' + activePage).fadeOut(200).delay(200).toggleClass('active');
+                $('#' + target).fadeIn(200).delay(200).toggleClass('active');
+            }
+        };
 
-function onGeolocateSuccess(position) {
-    alert('Latitude:  ' + position.coords.latitude + '\n' +
-          'Longitude: ' + position.coords.longitude);
-};
+        return {
+            init: function() {
+                // Need these to not allow overscrolling
+                $(window).bind('touchmove', function(e) { e.preventDefault(); });
+                $('body').on('touchmove','.scrollable', function(e) { e.stopPropagation(); });
 
-function onGeolocateError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
+                initSnap();
+                $('.menu-link').on('click', toggleMenu);
+                $('.menu').on('click', 'a', togglePages);
+            }
+        };
+    })();
+
+    // Cards
+    var Cards = (function() {
+        var navigateCards = function(e) {
+            e.preventDefault();
+
+            var action = $(this).attr('data-navigate');
+            var $thisCard = $(this).closest('.card');
+
+            if (action === 'next') {
+                $thisCard.toggleClass('prev active')
+                         .next()
+                         .toggleClass('next active')
+                         .nextAll();
+            } else if (action === 'prev') {
+                $thisCard.toggleClass('next active')
+                         .prev()
+                         .toggleClass('prev active')
+                         .nextAll();
+            } else if (action === 'close') {
+                $thisCard.closest('.overlay')
+                         .fadeOut(400);
+            }
+        };
+
+        return {
+            init: function() {
+                $('.card a[data-navigate]').on('click', navigateCards);
+            }
+        };
+    })();
+
+    // Initialize
+    var init = function () {
+        UI.init();
+        Cards.init();
+    };
+
+    return {
+        init: init
+    };
+})();
+
+jQuery(function ($) {
+    WR.App.init();
+});
