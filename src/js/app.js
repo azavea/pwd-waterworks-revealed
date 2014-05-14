bb02280b5e208bd6ef2502160c9f452926741ac0
Matthew McFarland <mmcfarland@azavea.com>
2014-05-14 15:28:50 -0400

    Demonstrate use of camera, display photo
    A reusable object to initiate a native camera and either set the
    source of a local <img> and/or return the path to the saved file.
    Allows all options of the PhoneGap camera API to be passed along.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,21 @@
 'use strict';
 
+var $ = require('jquery');
+
 module.exports = {
     init: function () {
         require('./ui').init();
         require('./cards').init();
+
+        // Demonstrate use of photocapture with optional callback
+        var PhotoCapture = require('./photoCapture'),
+            display = document.getElementById('pic'),
+            camera = new PhotoCapture(display);
+
+        $('a[data-activity="photo"]').on('click', function() {
+            camera.snap(function(path) {
+                console.log('photo loaded from: ' + path);
+            });
+        });
     }
 };
