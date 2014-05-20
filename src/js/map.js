94ee3fb43d8f50e7105c85ec7d8ba838f8398a5f
Matthew McFarland <mmcfarland@azavea.com>
2014-05-20 11:13:45 -0400

    Show user or mock position on map
    Using the geolocation feature to stream location updates to the map
    for marker display.  By setting mockLocations: true, you can also
    activate mock user locations on the map by tapping.

--- src/js/map.js
+++ src/js/map.js
@@ -1,18 +1,35 @@
 "use strict";
 
-var L = require('leaflet');
+var L = require('leaflet'),
+    Bacon = require('baconjs'),
+    _ = require('lodash'),
+    locationStream = require('./geolocator');
 
-function setupMap() {
-    try {
-        var map = L.map('tour-map').setView([39.9665675,-75.1834254], 18);
+function setupMap(options) {
+        var map = L.map('tour-map').setView([39.9665675,-75.1834254], 18),
+            opts = _.extend({ map: map }, options),
+            updateMarker = setupMarker(map),
+            loc = locationStream(opts).map(toLatLng);
 
         L.tileLayer('tiles/{z}/{x}/{y}.png', {maxZoom: 18}).addTo(map);
+        loc.onValue(updateMarker);
 
-    } catch (e) {
-        console.log(e);
-        alert(e);
-    }
 }
 
 
-module.exports = setupMap;
+function toLatLng(position) {
+    return L.latLng([position.coords.latitude, position.coords.longitude]);
+}
+
+function setupMarker(map) {
+    var icon = L.icon({iconUrl: 'img/location.png'}),
+        userMarker = L.marker([0,0],{icon: icon, clickable: false}).addTo(map);
+
+    return function(latLng) {
+        userMarker.setLatLng(latLng);
+    };
+}
+
+module.exports = {
+    init: setupMap
+};
