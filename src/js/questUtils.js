3fd4fded9f088a814f2ce2b5f09c05120b94fcf2
Laurence Liss <lliss@azavea.com>
2015-05-04 15:37:21 -0400

    Surpress error looking for attribute of null.

--- src/js/questUtils.js
+++ src/js/questUtils.js
@@ -10,18 +10,18 @@ exports = module.exports = {
 
     noQuestsStarted: function(zone) {
         // Returns true if no quests have ever been started in this zone
-        return !_.contains(zone.status, exports.STATUS_STARTED) &&
+        return zone && !_.contains(zone.status, exports.STATUS_STARTED) &&
                !_.contains(zone.status, exports.STATUS_FINISHED);
     },
 
     questInProgress: function(zone) {
         // Returns true if there is a quest in progress in this zone
-        return _.contains(zone.status, exports.STATUS_STARTED);
+        return zone && _.contains(zone.status, exports.STATUS_STARTED);
     },
 
     allQuestsDone: function(zone) {
         // Returns true if all quests are finished
-        return !_.contains(zone.status, exports.STATUS_STARTED) &&
+        return zone && !_.contains(zone.status, exports.STATUS_STARTED) &&
                !_.contains(zone.status, exports.STATUS_NOT_STARTED);
     },
 
