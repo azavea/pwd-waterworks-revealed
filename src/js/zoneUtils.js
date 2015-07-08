47d5a993d502111befca43d9ef862dd7347dc686
Casey Thomas <cthomas@azavea.com>
2015-07-08 09:26:57 -0400

    Add quest progress indicator
    Add a small UI element that indicates how many zones in the
    current quest are completed. If zones are hidden because
    the extent of the map is cropping them out, this helps
    the user see that there may be additional zones to visit.
    
    * Update quest progress whenever a zone status is changed.
    * Wire element to open quest details page on tap.
    * Add basic/demo CSS for element.
    
    Refs #102

--- src/js/zoneUtils.js
+++ src/js/zoneUtils.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports = module.exports = {
-    STATUS_NOT_STARTED: 1,
-    STATUS_FINISHED: 2,
+    STATUS_NOT_STARTED: 0,
+    STATUS_FINISHED: 1,
 
     zoneFinished: function(zone) {
         return zone && zone.status === exports.STATUS_FINISHED;
